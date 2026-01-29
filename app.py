00e5292fd787f8eb339b649c99676b4e1c02d54c
emiliomaj60-lang <emiliomaj60@gmail.com>
2026-01-29 18:15:36 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/attivita")
+def attivita():
+    return render_template("attivita.html")
+
+@app.route("/verbali")
+def verbali():
+    return render_template("verbali.html")
+
+@app.route("/iscritti")
+def iscritti():
+    return render_template("iscritti.html")
+
+@app.route("/emergenze")
+def emergenze():
+    return render_template("emergenze.html")
+
+@app.route("/pagina4")
+def pagina4():
+    return render_template("pagina4.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
