de4cf5cb7799400c1c6c1ef4e5ca35594c2535d6
emiliomaj60-lang <emiliomaj60@gmail.com>
2026-02-01 16:48:45 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -359,12 +359,16 @@ def send_alert_group():
             except WebPushException as e:
                 print("Errore invio notifica:", e)
 
+        # 🔥 AGGIORNO IL FILE ALLERTA PER LA BARRA E IL MENU
+        with open("static/allerta.txt", "w", encoding="utf-8") as f:
+            f.write(f"colore: {livello}\n")
+            f.write(f"messaggio: {titolo} – {messaggio}")
+
         return "OK"
 
     except Exception as e:
         print("Errore generale:", e)
         return "ERRORE", 500
-
 
 @app.route("/debug_base")# --- prova ---
 def debug_base():
